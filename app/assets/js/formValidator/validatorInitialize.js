f9c681435c1143e00819ab952bb3553002747356
jarher <jefferh33@gmail.com>
2024-10-20 03:55:15 -0500

    refactoring app code

--- app/assets/js/formValidator/validatorInitialize.js
+++ app/assets/js/formValidator/validatorInitialize.js
@@ -20,25 +20,4 @@ class ValidatorInitialize {
   }
 }
 
-// ValidatorInitialize.prototype.validateRequiredProperties = function (data) {
-//   const requiredProperties = [
-//     "initialValues",
-//     "validatorSchema",
-//     "formEvents",
-//     "errorOutputSelector",
-//     "formControlAttribute",
-//   ];
-
-//   const missingProperties = requiredProperties.filter(
-//     (property) => !data.hasOwnProperty(property)
-//   );
-
-//   if (missingProperties.length > 0)
-//     throw new Error(
-//       `Missing required properties in form data: ${missingProperties.join(
-//         ", "
-//       )}`
-//     );
-// };
-
 export default ValidatorInitialize;
--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -13,14 +13,14 @@ const validatorSchema = {
   dayNumber: {
     type: "number",
     errors: {
-      number: "Must be a valid day",
+      message: "Must be a valid day",
       min: {
         value: 1,
-        error: "Must be a valid day",
+        message: "Must be a valid day",
       },
       max: {
         value: 2,
-        error: "Must be a valid day",
+        message: "Must be a valid day",
       },
       customValidation: (birth_day) => dayValidate(birth_day),
       required: "This field is required",
@@ -29,14 +29,14 @@ const validatorSchema = {
   monthNumber: {
     type: "number",
     errors: {
-      number: "Must be a valid month",
+      message: "Must be a valid month",
       min: {
         value: 1,
-        error: "Must be a valid month",
+        message: "Must be a valid month",
       },
       max: {
         value: 2,
-        error: "Must be a valid month",
+        message: "Must be a valid month",
       },
       customValidation: (birth_month) => monthValidate(birth_month),
       required: "This field is required",
@@ -45,14 +45,14 @@ const validatorSchema = {
   yearNumber: {
     type: "number",
     errors: {
-      number: "Must be a valid year",
+      message: "Must be a valid year",
       min: {
         value: 4,
-        error: "Must be a valid year",
+        message: "Must be a valid year",
       },
       max: {
         value: 4,
-        error: "Must be in the past",
+        message: "Must be in the past",
       },
       customValidation: (birth_year) => yearValidate(birth_year),
       required: "This field is required",
@@ -68,8 +68,8 @@ formValidation({
   },
   validatorSchema,
   formEvents: ["submit"],
-  errorOutputSelector: ".input-error",
-  formControlAttribute: "form-wrapper-invalid",
+  errorOutputClass: "input-error",
+  formControlClass: "form-wrapper-invalid",
   onSubmit: (values) => {
     const date = calculateDateDifference(values);
     //animates result date
